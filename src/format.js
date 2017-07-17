a4ae153789d3ccb05cea317e800734891dbecd1a
mirreal <zgkm181142521@gmail.com>
2017-07-17 10:21:00 +0800

    update: eslint fix

--- src/format.js
+++ src/format.js
@@ -1,22 +1,21 @@
 const _ = require('lodash')
 
-var STYLE  = {
-  "ZHIHU_HEADER_1"   : 'font-weight:bold;',
-  "EMAIL_HEADER_1"   : 'padding:10 5px;line-height:36px;font-size:23px;background-color:#f7df1e;font-weight:bold;color:#323330',
-  "EMAIL_HEADER_2"   : 'font-size:20px;color:#4e4e4e',
-  "EMAIL_LINK"       : 'display:block;font-size:18px;color:#0081b5;line-height:24px;text-decoration:underline;font-weight:normal',
-  "EMAIL_PARAGRAPH"  : 'font-size:14px;line-height:19px;margin-top:0px;margin-bottom:3px',
-  "EMAIL_BLOCKQUOTE" : 'font-size:14px;line-height:19px;margin-top:0px;margin-bottom:3px'
-};
+var STYLE = {
+  ZHIHU_HEADER_1: 'font-weight:bold;',
+  EMAIL_HEADER_1: 'padding:10 5px;line-height:36px;font-size:23px;background-color:#f7df1e;font-weight:bold;color:#323330',
+  EMAIL_HEADER_2: 'font-size:20px;color:#4e4e4e',
+  EMAIL_LINK: 'display:block;font-size:18px;color:#0081b5;line-height:24px;text-decoration:underline;font-weight:normal',
+  EMAIL_PARAGRAPH: 'font-size:14px;line-height:19px;margin-top:0px;margin-bottom:3px',
+  EMAIL_BLOCKQUOTE: 'font-size:14px;line-height:19px;margin-top:0px;margin-bottom:3px'
+}
 function formatZhihu(data) {
   let zhihuData = data.map(item => {
     if (_.isArray(item) && item[0] === 'header') {
-      if(item[1].level === 4){
+      if (item[1].level === 4) {
         return item[2]
-      }
-      else if(item[1].level === 1){
+      } else if (item[1].level === 1) {
         item[1].style = STYLE['ZHIHU_HEADER_1']
-        return item;
+        return item
       }
     }
 
@@ -32,11 +31,11 @@ function formatEmail(data) {
   let emailData = mdData.map(item => {
     if (_.isArray(item)) {
       if (item[0] === 'header' && item[1].level === 1) {
-        item[1].style = STYLE['EMAIL_HEADER_1'];
+        item[1].style = STYLE['EMAIL_HEADER_1']
       }
 
       if (item[0] === 'header' && item[1].level === 2) {
-        item[1].style = STYLE['EMAIL_HEADER_2'];
+        item[1].style = STYLE['EMAIL_HEADER_2']
       }
 
       // link style
